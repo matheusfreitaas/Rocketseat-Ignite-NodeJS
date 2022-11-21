31c7e3b2e007184e4def9422fea9fc5c67391d55
Matheus Freitas <matheusbfreitas@gmail.com>
2022-11-21 16:03:54 -0300

    Add rentalx

---
+++ Rentalx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -0,0 +1,20 @@
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) { }
+
+  execute({ name, description }: IRequest): void {
+    const categoryAlreadyExist = this.categoriesRepository.findByName(name);
+    if (categoryAlreadyExist) {
+      throw new Error('Category already exist');
+    }
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryUseCase };
---
+++ Rentalx/src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -0,0 +1,21 @@
+import { ISpecificationsRepository } from '../../repositories/ISpecificationsRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationUseCase {
+  constructor(private specificationsRepository: ISpecificationsRepository) { }
+
+  execute({ name, description }: IRequest) {
+    const specificationAlreadyExist =
+      this.specificationsRepository.findByName(name);
+    if (specificationAlreadyExist) {
+      throw new Error('Specification already exist');
+    }
+    this.specificationsRepository.create({ name, description });
+  }
+}
+
+export { CreateSpecificationUseCase };
